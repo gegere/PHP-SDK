a17b03ec32874807e083eef38916f2b9ef8f2412
Jason Gegere <jason@hgmail.com>
2014-04-16 12:37:09 -0500

    correct path to lib files

--- scripts/notifications/checkfront-json.php
+++ scripts/notifications/checkfront-json.php
@@ -1,7 +1,7 @@
 <?php
 	date_default_timezone_set('GMT');
-	require_once '../includes/db.class.php';
-	require_once '../includes/notifications.class.php';
+	require_once 'includes/db.class.php';
+	require_once 'includes/notifications.class.php';
 
 
 # COMMENTED CODE IS USED TO WRITE TO A FILE, GOOD FOR DEBUGGING
