f302d09a3aa529bbf20e02aed22c9ea750e61d21
Brett Goss <goss.brett@gmail.com>
2018-03-07 00:19:03 -0800

    Use field id instead of name

--- examples/cart/Form.php
+++ examples/cart/Form.php
@@ -102,7 +102,7 @@ class Form {
 	}
 
 	private function build_checkbox($id,$data) {
-		$html = "<input type='checkbox' name='{$name}' id='{$id}' value='1'";
+		$html = "<input type='checkbox' name='{$id}' id='{$id}' value='1'";
 		if($data['value']) $html .= ' checked="checked"';
 		$html .= "/>";
 		return $html;
